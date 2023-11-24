c0659b338750c592a6326683d55fdc12473f3d9d
Aponia <brianmv@uci.edu>
2023-11-24 13:05:08 -0800

    feat: ✨ provision route53

--- tools/cdk/index.ts
+++ tools/cdk/index.ts
@@ -1,5 +1,7 @@
 import { SvelteKit } from "@svelte.kit/cdk";
 import { App, Stack } from "aws-cdk-lib";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
 
 async function main() {
   const stackName = `sparkle-starter-canary`;
@@ -8,7 +10,22 @@ async function main() {
 
   const stack = new Stack(app, stackName);
 
-  new SvelteKit(stack, stackName);
+  const sveltekit = new SvelteKit(stack, stackName);
+
+  const stage = process.env.STAGE ?? "dev";
+
+  if (!process.env.CERTIFICATE_ARN) {
+    throw new Error("Certificate ARN not provided.");
+  }
+
+  new ARecord(stack, "a-record", {
+    zone: HostedZone.fromHostedZoneAttributes(stack, "peterportal-hosted-zone", {
+      zoneName: `${stage === "prod" ? "" : `${stage}.`}zotmeet.com`,
+      hostedZoneId: process.env.HOSTED_ZONE_ID ?? "",
+    }),
+    recordName: `${stage === "prod" ? "" : `${stage}.`}api-next`,
+    target: RecordTarget.fromAlias(new CloudFrontTarget(sveltekit.distribution)),
+  });
 }
 
 main();
